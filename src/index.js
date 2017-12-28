12486d21c0a3688030ad37675b475888ae87619c
Vladimir Rancic <vladimir@Vladimirs-MacBook-Pro.local>
2017-12-28 12:10:21 +0100

    index.js update

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import './index.css';
+//import { BrowserRouter, Route } from 'react-router-dom';
 
 import App from './App';
 import reducers from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  // <BrowserRouter>
+  //   <Route path="" component={} />
+  //   <Route path="" component={} />
+  // </BrowserRouter>,
+  <App />, document.getElementById('root')
+);
 registerServiceWorker();
