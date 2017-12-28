afbce88bfdee37990a37d92a12271b3a1032eea2
Vladimir Rancic <vladimir@Vladimirs-MacBook-Pro.local>
2017-12-28 11:55:34 +0100

    Add redux and router packages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import './index.css';
+
 import App from './App';
+import reducers from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
